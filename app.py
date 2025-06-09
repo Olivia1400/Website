0a7cbdac2d37020dbce86c37ad5082183a25be1d
Olivia1400 <livvyanderson2006@gmail.com>
2025-06-09 10:46:56 +0000

    small changes and additions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template # type: ignore
-
+from flask import Flask, render_template, request,redirect,url_for,session
+import 
 app = Flask(__name__)
 
 
@@ -7,6 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/SignUp', methods=['GET','POST'])
+def SignUp():
+    
+
+@app.route('/LogIn', methods=['GET','POST'])
+def LogIn():
+    
+
+@app.route('/dashboard')
+def dashboard():
+    
+
+@app.route('/Signout')
+def SignOut():
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
