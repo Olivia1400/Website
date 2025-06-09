a02ee388abb07fda35d01409ff01e21dd5c84e20
Olivia1400 <livvyanderson2006@gmail.com>
2025-06-09 10:21:29 +0000

    Uploading of new DB addition

--- CreateDB.py
+++ CreateDB.py
@@ -1,12 +1,36 @@
 import sqlite3
 
+
+#! selects command type to be used in following function
+def select_command():
+    try:
+        sqlite3.connect('database.db')
+    except sqlite3.OperationalError:
+        print("Database not found. Please create the database first.")
+        start(cmd_Type='create')
+    else:
+        print("Database connection established successfully.")
+        start(cmd_Type='upload')
+
+#! This is the start function for determining creating or uploading to db
+
+def start(cmd_Type):
+    if cmd_Type == 'create':
+        run_database(create_command())
+        print("Database created successfully.")
+    elif cmd_Type == 'upload':
+        run_database(upload_command())
+        print("Data uploaded successfully.")
+    else:
+        print("Invalid command type. Please use 'create' or 'upload'.")
+
+#! Runs THe command for the database
 def run_database(command):
     
     cursor = sqlite3.connect('database.db')
     cursor.execute(command)
     cursor.commit()
     cursor.close()
-
 
 #! Creates the database and the table if it does not exist.
 
@@ -25,17 +49,15 @@ def create_command():
 
 #! Runs the database upload command.
 def upload_command():
+    
     command = (''' INSERT INTO Database 
             (username, password, age, gender) 
             VALUES (?, ?, ?, ?)''')
     
-    
     return command
-
 
 
 #! Runs the database creation and uploading of any data into the database.
 
 if __name__ == '__main__':
-    run_database(create_command())
-    print("Database executing command.")
+    select_command()
