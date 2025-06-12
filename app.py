11dc7cd4223f45f7ad7637756d4feabb368a8d21
Olivia1400 <livvyanderson2006@gmail.com>
2025-06-12 09:18:25 +0000

    Added onto it

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request,redirect,url_for,session
-import 
+import os
+import sqlite3
+
 app = Flask(__name__)
+app.secret_key = '12'
+db_locale = 'users.db'
 
 
 
@@ -10,7 +14,16 @@ def index():
 
 @app.route('/SignUp', methods=['GET','POST'])
 def SignUp():
-    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = hashlib.sha256(request.form['password'].encode()).hexdigest()
+        connection = sqlite3.connect(dblocale)
+        cursor = connection.cursor()
+        try:
+            cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+            connection.commit()
+            flash()
+            
 
 @app.route('/LogIn', methods=['GET','POST'])
 def LogIn():
@@ -27,5 +40,3 @@ def SignOut():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
-
-
